2eae01a61680da1db54bf32fe83fc9524a199bbd
Florian Maas <florian.maas@ingka.ikea.com>
2022-09-03 14:23:47 +0200

    added the directory argument to the CLI

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,20 +1,8 @@
-import os
 import shlex
 import shutil
 import subprocess
-from contextlib import contextmanager
 
-import pytest
-
-
-@contextmanager
-def run_within_dir(path: str):
-    oldpwd = os.getcwd()
-    os.chdir(path)
-    try:
-        yield
-    finally:
-        os.chdir(oldpwd)
+from deptry.utils import run_within_dir
 
 
 def test_cli_returns_error(tmp_path):
@@ -28,12 +16,12 @@ def test_cli_returns_error(tmp_path):
 
     with run_within_dir(str(tmp_path_proj)):
         subprocess.check_call(shlex.split("poetry install --no-interaction --no-root")) == 0
-        result = subprocess.run(shlex.split("poetry run deptry check"), capture_output=True, text=True)
+        result = subprocess.run(shlex.split("poetry run deptry check ."), capture_output=True, text=True)
         assert result.returncode == 1
         assert result.stderr == "pyproject.toml contains obsolete dependencies: ['isort']\n"
 
         result = subprocess.run(
-            shlex.split("poetry run deptry check --ignore-notebooks"), capture_output=True, text=True
+            shlex.split("poetry run deptry check . --ignore-notebooks"), capture_output=True, text=True
         )
         assert result.returncode == 1
         assert result.stderr == "pyproject.toml contains obsolete dependencies: ['cookiecutter-poetry', 'isort']\n"
@@ -50,7 +38,7 @@ def test_cli_returns_no_error(tmp_path):
 
     with run_within_dir(str(tmp_path_proj)):
         subprocess.check_call(shlex.split("poetry install --no-interaction --no-root")) == 0
-        result = subprocess.run(shlex.split("poetry run deptry check"), capture_output=True, text=True)
+        result = subprocess.run(shlex.split("poetry run deptry check ."), capture_output=True, text=True)
         assert result.returncode == 0
 
 
@@ -66,7 +54,7 @@ def test_cli_argument_overwrites_pyproject_toml_argument(tmp_path):
 
     with run_within_dir(str(tmp_path_proj)):
         subprocess.check_call(shlex.split("poetry install --no-interaction --no-root")) == 0
-        result = subprocess.run(shlex.split("poetry run deptry check -i click"), capture_output=True, text=True)
+        result = subprocess.run(shlex.split("poetry run deptry check . -i click"), capture_output=True, text=True)
         assert result.returncode == 1
         assert result.stderr == "pyproject.toml contains obsolete dependencies: ['isort', 'toml']\n"
 
