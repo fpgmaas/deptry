2af7623637378a45be8210fb3921927ab18c218d
Florian Maas <fpgmaas@gmail.com>
2022-09-04 12:37:19 +0200

    Added functionality to support parsing imports within if/else statements (#23)

--- tests/test_import_parser.py
+++ tests/test_import_parser.py
@@ -5,12 +5,28 @@ from deptry.import_parser import ImportParser
 
 
 def test_import_parser_py():
-    imported_modules = ImportParser()._get_imported_modules_from_file(Path("tests/data/some_imports.py"))
+    imported_modules = ImportParser().get_imported_modules_from_file(Path("tests/data/some_imports.py"))
     assert set(imported_modules) == set(["os", "pathlib", "typing", "pandas", "numpy"])
 
 
 def test_import_parser_ipynb():
-    imported_modules = ImportParser()._get_imported_modules_from_file(
+    imported_modules = ImportParser().get_imported_modules_from_file(
         Path("tests/data/projects/project_with_obsolete/src/notebook.ipynb")
     )
     assert set(imported_modules) == set(["click", "pandas", "numpy", "cookiecutter_poetry"])
+
+
+def test_import_parser_ifelse():
+    imported_modules = ImportParser().get_imported_modules_from_str(
+        """
+x=1
+import numpy
+if x>0:
+    import pandas
+elif x<0:
+    from typing import List
+else:
+    import logging
+"""
+    )
+    assert set(imported_modules) == set(["numpy", "pandas", "typing", "logging"])
