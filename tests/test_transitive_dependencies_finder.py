39ac2d40334ce2438e985e960d918c561119bf39
Florian Maas <fpgmaas@gmail.com>
2022-09-09 10:27:16 +0200

    Revert "added support for python 3.11 (#58)" (#60)
    This reverts commit bd3bfbd0e03af6ee7617daab62f23caa5cd7bd22.

--- tests/test_transitive_dependencies_finder.py
+++ tests/test_transitive_dependencies_finder.py
@@ -5,13 +5,13 @@ from deptry.module import ModuleBuilder
 
 def test_simple():
     """
-    black is in testing environment which requires platformdirs, so platformdirs should be found as transitive.
+    matplotlib is in testing environment which requires pillow, so pillow should be found as transitive.
     """
     dependencies = []
-    modules = [ModuleBuilder("platformdirs", dependencies).build()]
+    modules = [ModuleBuilder("pillow", dependencies).build()]
     deps = TransitiveDependenciesFinder(imported_modules=modules, dependencies=dependencies).find()
     assert len(deps) == 1
-    assert deps[0] == "platformdirs"
+    assert deps[0] == "Pillow"
 
 
 def test_simple_with_ignore():
