32b472f0887a6adb18dc8800a254ab6470241308
Florian Maas <fpgmaas@gmail.com>
2022-09-13 08:15:59 +0200

    solved an issue where missing dev dependencies were added to the list as None (#99)
    * solved an issue where missing dev depenencies were added to the list as None
    * Added unit test

--- deptry/issue_finders/misplaced_dev.py
+++ deptry/issue_finders/misplaced_dev.py
@@ -14,28 +14,58 @@ class MisplacedDevDependenciesFinder:
     """
 
     def __init__(
-        self, imported_modules: List[Module], dependencies: List[Dependency], ignore_misplaced_dev: List[str] = []
+        self,
+        imported_modules: List[Module],
+        dependencies: List[Dependency],
+        dev_dependencies: List[Dependency],
+        ignore_misplaced_dev: List[str] = [],
     ) -> None:
         self.imported_modules = imported_modules
         self.dependencies = dependencies
+        self.dev_dependencies = dev_dependencies
         self.ignore_misplaced_dev = ignore_misplaced_dev
 
     def find(self) -> List[str]:
+        """
+        In this function, we use 'corresponding_package_name' instead of module.package, since it can happen that a
+        development dependency is not installed, but it's still found to be used in the codebase, due to simple name matching.
+        In that case, it's added under module.dev_top_levels. _get_package_name is added for these edge-cases.
+        """
         logging.debug("\nScanning for incorrect development dependencies...")
-        dev_dependencies = []
+        misplaced_dev_dependencies = []
         for module in self.imported_modules:
             logging.debug(f"Scanning module {module.name}...")
-            if self._is_development_dependency(module):
-                dev_dependencies.append(module.package)
-        return dev_dependencies
+            corresponding_package_name = self._get_package_name(module)
+            if corresponding_package_name:
+                if self._is_development_dependency(module, corresponding_package_name):
+                    misplaced_dev_dependencies.append(corresponding_package_name)
+        return misplaced_dev_dependencies
 
-    def _is_development_dependency(self, module: Module) -> bool:
+    def _is_development_dependency(self, module: Module, corresponding_package_name: str) -> bool:
         if module.is_dev_dependency:
             if module.name in self.ignore_misplaced_dev:
                 logging.debug(
-                    f"Module '{module.package}' found to be a misplaced development dependency, but ignoring."
+                    f"Module '{corresponding_package_name}' found to be a misplaced development dependency, but ignoring."
                 )
             else:
-                logging.debug(f"Dependency '{module.package}' marked as a misplaced development dependency.")
+                logging.debug(
+                    f"Dependency '{corresponding_package_name}' marked as a misplaced development dependency."
+                )
                 return True
         return False
+
+    def _get_package_name(self, module: Module):
+        if module.package:
+            return module.package
+        if module.dev_top_levels:
+            if len(module.dev_top_levels) > 1:
+                logging.debug(
+                    f"Module {module.name} is found in the top-level module names of multiple development dependencies. Skipping."
+                )
+            elif len(module.dev_top_levels) == 0:
+                logging.debug(
+                    f"Module {module.name} has no metadata and it is not found in any top-level module names. Skipping."
+                )
+            else:
+                return module.dev_top_levels[0]
+        return None
