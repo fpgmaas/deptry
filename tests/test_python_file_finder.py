b72cdd9db32226b4bee09021b330cb4177085dda
Florian Maas <florian.maas@ingka.ikea.com>
2022-09-17 11:07:04 +0200

    improved file scanner, now uses regex

--- tests/test_python_file_finder.py
+++ tests/test_python_file_finder.py
@@ -1,29 +1,100 @@
-import os
-import shlex
-import shutil
-import subprocess
 from contextlib import contextmanager
 from pathlib import Path
+from typing import Dict, List
 
 import pytest
 
 from deptry.python_file_finder import PythonFileFinder
+from deptry.utils import run_within_dir
 
 
-def test_find_only_py_files():
+def create_files_from_list_of_dicts(paths: List[Dict[str, str]]):
     """
-    Should only find src/main.py
+    Takes as input an argument paths, which is a list of dicts. Each dict should have two key's;
+    'dir' to denote a directory and 'file' to denote the file name. This function creates all files
+    within their corresponding directories.
     """
-    files = PythonFileFinder(ignore_notebooks=True).get_all_python_files_in(Path("tests/data/example_project"))
-    assert len(files) == 1
-    assert "main.py" in (str(files[0]))
+    for path in paths:
+        Path(path["dir"]).mkdir(parents=True, exist_ok=True)
+        open(Path(path["dir"]) / Path(path["file"]), "w").close()
 
 
-def test_find_py_and_ipynb_files():
+def test_simple(tmp_path):
+    with run_within_dir(tmp_path):
+        paths = [
+            {"dir": "dir/subdir", "file": "file1.py"},
+            {"dir": "dir/subdir", "file": "file2.py"},
+            {"dir": "dir/subdir", "file": "file3.py"},
+            {"dir": "other_dir/subdir", "file": "file1.py"},
+            {"dir": "other_dir/subdir", "file": "file2.py"},
+        ]
+        create_files_from_list_of_dicts(paths)
+
+        files = PythonFileFinder(exclude=[".venv", "other_dir"]).get_all_python_files_in(".")
+        assert len(files) == 3
+        assert "dir/subdir/file2.py" in [str(file) for file in files]
+
+
+def test_only_matches_start(tmp_path):
     """
-    Should find src/main.py and src/notebook.ipynb
+    Test the adding 'subdir' as exclude argument does not also exclude dir/subdir.
     """
-    files = PythonFileFinder(ignore_notebooks=False).get_all_python_files_in(Path("tests/data/example_project"))
-    assert len(files) == 2
-    assert any(["main.py" in str(x) for x in files])
-    assert any(["notebook.ipynb" in str(x) for x in files])
+    with run_within_dir(tmp_path):
+        paths = [
+            {"dir": "dir/subdir", "file": "file1.py"},
+            {"dir": "dir/subdir", "file": "file2.py"},
+            {"dir": "dir/subdir", "file": "file3.py"},
+            {"dir": "subdir", "file": "file1.py"},
+            {"dir": "subdir", "file": "file2.py"},
+        ]
+        create_files_from_list_of_dicts(paths)
+
+        files = PythonFileFinder(exclude=["subdir"]).get_all_python_files_in(".")
+        assert len(files) == 3
+        assert "dir/subdir/file2.py" in [str(file) for file in files]
+
+
+def test_matches_ipynb(tmp_path):
+    """
+    Test the adding 'subdir' as exclude argument does not also exclude dir/subdir.
+    """
+    with run_within_dir(tmp_path):
+        paths = [
+            {"dir": "dir/subdir", "file": "file1.ipynb"},
+        ]
+        create_files_from_list_of_dicts(paths)
+
+        files = PythonFileFinder(exclude=["subdir"], ignore_notebooks=False).get_all_python_files_in(".")
+        assert len(files) == 1
+        assert "dir/subdir/file1.ipynb" in [str(file) for file in files]
+        files = PythonFileFinder(exclude=["subdir"], ignore_notebooks=True).get_all_python_files_in(".")
+        assert len(files) == 0
+
+
+def test_regex_argument(tmp_path):
+    """
+    Test the adding 'subdir' as exclude argument does not also exclude dir/subdir.
+    """
+    with run_within_dir(tmp_path):
+        paths = [
+            {"dir": "dir/subdir", "file": "file1.py"},
+            {"dir": "dir/subdir", "file": "file2.py"},
+            {"dir": "dir/subdir", "file": "file3.py"},
+            {"dir": "other_dir/subdir", "file": "file1.py"},
+            {"dir": "other_dir/subdir", "file": "file2.py"},
+            {"dir": ".cache", "file": "file1.py"},
+            {"dir": ".cache", "file": "file2.py"},
+        ]
+        create_files_from_list_of_dicts(paths)
+
+        files = PythonFileFinder(exclude=[".*file1"], ignore_notebooks=False).get_all_python_files_in(".")
+        assert len(files) == 4
+        assert not any(["file1" in str(file) for file in files])
+
+        files = PythonFileFinder(exclude=[".cache", "other.*subdir"], ignore_notebooks=False).get_all_python_files_in(
+            "."
+        )
+        assert len(files) == 3
+        assert not any(["other_dir" in str(file) for file in files])
+        assert not any([".cache" in str(file) for file in files])
+        assert "dir/subdir/file2.py" in [str(file) for file in files]
