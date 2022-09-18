343315d24deb05960c7238886cc0d0a4ff1ba11c
Florian Maas <fpgmaas@gmail.com>
2022-09-18 09:29:52 +0200

    Added a method to extract top level module names from RECORD (#116)
    * Added a method to extract top level module names from RECORD

--- tests/test_dependency.py
+++ tests/test_dependency.py
@@ -1,4 +1,7 @@
+from unittest.mock import patch
+
 from deptry.dependency import Dependency
+from deptry.utils import import_importlib_metadata
 
 
 def test_simple_dependency():
@@ -11,3 +14,49 @@ def test_create_default_top_level_if_metadata_not_found():
     dependency = Dependency("Foo-bar")
     assert dependency.name == "Foo-bar"
     assert dependency.top_levels == set(["foo_bar"])
+
+
+def test_read_top_level_from_top_level_txt():
+    """
+    Read the top-levels.txt file
+    """
+
+    class MockDistribution:
+        def __init__(self):
+            pass
+
+        def read_text(self, file_name):
+            return "foo\nbar"
+
+    with patch("deptry.dependency.metadata.distribution") as mock:
+        mock.return_value = MockDistribution()
+        dependency = Dependency("Foo-bar")
+        assert dependency.name == "Foo-bar"
+        assert dependency.top_levels == set(["foo_bar", "foo", "bar"])
+
+
+def test_read_top_level_from_record():
+    """
+    Verify that if top-level.txt not found, an attempt is made to extract top-level module names from
+    the metadata RECORD file.
+    """
+
+    class MockDistribution:
+        def __init__(self):
+            pass
+
+        def read_text(self, file_name):
+            if file_name == "top-level.txt":
+                return None
+            elif file_name == "RECORD":
+                return """black/trans.cpython-39-darwin.so,sha256=<HASH>
+black/trans.py,sha256=<HASH>
+blackd/__init__.py,sha256=<HASH>
+blackd/__main__.py,sha256=<HASH>
+                """
+
+    with patch("deptry.dependency.metadata.distribution") as mock:
+        mock.return_value = MockDistribution()
+        dependency = Dependency("Foo-bar")
+        assert dependency.name == "Foo-bar"
+        assert dependency.top_levels == set(["foo_bar", "black", "blackd"])
