37a4e38af749be9d51c722a4c03d0724638a7b4e
Mathieu Kniewallner <mathieu.kniewallner@gmail.com>
2022-11-02 00:08:40 +0100

    refactor(pdm): subclass PEP 621 dependency getter

--- deptry/dependency_getter/pdm.py
+++ deptry/dependency_getter/pdm.py
@@ -1,31 +1,26 @@
 import logging
-import re
-from typing import Dict, List, Optional
+from dataclasses import dataclass
+from typing import Dict, List
 
 from deptry.dependency import Dependency
-from deptry.dependency_getter.base import DependenciesExtract, DependencyGetter
+from deptry.dependency_getter.base import DependenciesExtract
+from deptry.dependency_getter.pep_621 import PEP621DependencyGetter
 from deptry.utils import load_pyproject_toml
 
 
-class PDMDependencyGetter(DependencyGetter):
+@dataclass
+class PDMDependencyGetter(PEP621DependencyGetter):
     """
     Class to get dependencies that are specified according to PEP 621 from a `pyproject.toml` file for a project that uses PDM for its dependency management.
     """
 
     def get(self) -> DependenciesExtract:
-        dependencies = self._get_pdm_dependencies()
-        self._log_dependencies(dependencies)
+        pep_621_dependencies_extract = super().get()
 
         dev_dependencies = self._get_pdm_dev_dependencies()
         self._log_dependencies(dev_dependencies, is_dev=True)
 
-        return DependenciesExtract(dependencies, dev_dependencies)
-
-    @classmethod
-    def _get_pdm_dependencies(cls) -> List[Dependency]:
-        pyproject_data = load_pyproject_toml()
-        dependency_strings: List[str] = pyproject_data["project"]["dependencies"]
-        return cls._extract_dependency_objects_from(dependency_strings)
+        return DependenciesExtract(pep_621_dependencies_extract.dependencies, dev_dependencies)
 
     @classmethod
     def _get_pdm_dev_dependencies(cls) -> List[Dependency]:
@@ -52,31 +47,4 @@ class PDMDependencyGetter(DependencyGetter):
         except KeyError:
             logging.debug("No section [tool.pdm.dev-dependencies] found in pyproject.toml")
 
-        return cls._extract_dependency_objects_from(dev_dependency_strings)
-
-    @classmethod
-    def _extract_dependency_objects_from(cls, pdm_dependencies: List[str]) -> List[Dependency]:
-        dependencies = []
-        for spec in pdm_dependencies:
-            # An example of a spec is `"tomli>=1.1.0; python_version < \"3.11\""`
-            name = cls._find_dependency_name_in(spec)
-            if name:
-                optional = cls._is_optional(spec)
-                conditional = cls._is_conditional(spec)
-                dependencies.append(Dependency(name, conditional=conditional, optional=optional))
-        return dependencies
-
-    @staticmethod
-    def _is_optional(dependency_specification: str) -> bool:
-        return bool(re.findall(r"\[([a-zA-Z0-9-]+?)\]", dependency_specification))
-
-    @staticmethod
-    def _is_conditional(dependency_specification: str) -> bool:
-        return ";" in dependency_specification
-
-    @staticmethod
-    def _find_dependency_name_in(spec: str) -> Optional[str]:
-        match = re.search("[a-zA-Z0-9-_]+", spec)
-        if match:
-            return match.group(0)
-        return None
+        return cls._extract_pep_508_dependencies(dev_dependency_strings)
