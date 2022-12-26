de6875e04d258df4a685f8027d9d62194638ac11
Mathieu Kniewallner <mathieu.kniewallner@gmail.com>
2022-12-26 23:45:34 +0100

    test(cli): add tests for config in different path

---
+++ tests/cli/test_cli_pyproject_different_directory.py
@@ -0,0 +1,30 @@
+import shlex
+import shutil
+import subprocess
+from pathlib import Path
+
+import pytest
+from _pytest.tmpdir import TempPathFactory
+
+from tests.utils import run_within_dir
+
+
+@pytest.fixture(scope="session")
+def pep_621_dir_with_pyproject_different_directory(tmp_path_factory: TempPathFactory) -> Path:
+    tmp_path_proj = tmp_path_factory.getbasetemp() / "project_with_pyproject_different_directory"
+    shutil.copytree("tests/data/project_with_pyproject_different_directory", str(tmp_path_proj))
+    with run_within_dir(tmp_path_proj):
+        assert subprocess.check_call(shlex.split("pip install ."), cwd="a_sub_directory") == 0
+    return tmp_path_proj
+
+
+def test_cli_with_pyproject_different_directory(pep_621_dir_with_pyproject_different_directory: Path) -> None:
+    with run_within_dir(pep_621_dir_with_pyproject_different_directory):
+        result = subprocess.run(
+            shlex.split("deptry --config a_sub_directory/pyproject.toml src"), capture_output=True, text=True
+        )
+        assert result.returncode == 1
+        assert (
+            "The project contains obsolete dependencies:\n\n\tisort\n\tmypy\n\tpytest\n\trequests\n\n" in result.stderr
+        )
+        assert "There are dependencies missing from the project's list of dependencies:\n\n\twhite\n\n" in result.stderr
---
+++ tests/data/project_with_pyproject_different_directory/src/project_with_src_directory/foo.py
@@ -0,0 +1,11 @@
+from os import chdir, walk
+from pathlib import Path
+
+import black
+import click
+import white as w
+from urllib3 import contrib
+
+
+def a_local_method():
+    ...
