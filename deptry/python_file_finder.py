053746718fc6611dbc7104af035be4c84f555d15
Mathieu Kniewallner <mathieu.kniewallner@gmail.com>
2022-12-30 01:07:57 +0100

    refactor(finder): move ignore checks to dedicated functions

--- deptry/python_file_finder.py
+++ deptry/python_file_finder.py
@@ -5,6 +5,7 @@ import os
 import re
 from dataclasses import dataclass
 from pathlib import Path
+from typing import Pattern
 
 
 @dataclass
@@ -31,15 +32,21 @@ class PythonFileFinder:
         for root_str, dirs, files in os.walk(directory, topdown=True):
             root = Path(root_str)
 
-            if self.exclude and ignore_regex.match(str(root)):
+            if self._is_directory_ignored(root, ignore_regex):
                 dirs[:] = []
                 continue
 
             for file_str in files:
                 file = root / file_str
-                if file.suffix in file_lookup_suffixes and (not self.exclude or not ignore_regex.match(str(file))):
+                if not self._is_file_ignored(file, file_lookup_suffixes, ignore_regex):
                     source_files.append(file)
 
         logging.debug("Python files to scan for imports:\n%s\n", "\n".join([str(file) for file in source_files]))
 
         return source_files
+
+    def _is_directory_ignored(self, directory: Path, ignore_regex: Pattern[str]) -> bool:
+        return bool(self.exclude and ignore_regex.match(str(directory)))
+
+    def _is_file_ignored(self, file: Path, file_lookup_suffixes: set[str], ignore_regex: Pattern[str]) -> bool:
+        return bool(file.suffix not in file_lookup_suffixes or (self.exclude and ignore_regex.match(str(file))))
