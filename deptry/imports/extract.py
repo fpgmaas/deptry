ec4451c1ee217779a0d3b050bef5ffc29562f04c
Mathieu Kniewallner <mathieu.kniewallner@gmail.com>
2023-01-06 23:32:44 +0100

    refactor(extract): move class guessing to dedicated function

--- deptry/imports/extract.py
+++ deptry/imports/extract.py
@@ -5,6 +5,7 @@ import logging
 from pathlib import Path
 
 from deptry.imports.extractors import NotebookImportExtractor, PythonImportExtractor
+from deptry.imports.extractors.base import ImportExtractor
 
 # setuptools is usually available by default, so often not specified in dependencies.
 _FILTERED_OUT_MODULES = {"setuptools"}
@@ -24,14 +25,17 @@ def get_imported_modules_for_list_of_files(list_of_files: list[Path]) -> list[st
 def get_imported_modules_from_file(path_to_file: Path) -> set[str]:
     logging.debug(f"Scanning {path_to_file}...")
 
-    if path_to_file.suffix == ".ipynb":
-        modules = NotebookImportExtractor(path_to_file).extract_imports()
-    else:
-        modules = PythonImportExtractor(path_to_file).extract_imports()
+    modules = _get_extractor_class(path_to_file)(path_to_file).extract_imports()
 
     logging.debug(f"Found the following imports in {str(path_to_file)}: {modules}")
 
     return modules
+
+
+def _get_extractor_class(path_to_file: Path) -> type[ImportExtractor]:
+    if path_to_file.suffix == ".ipynb":
+        return NotebookImportExtractor
+    return PythonImportExtractor
 
 
 def _filter_out_modules(modules: set[str]) -> set[str]:
