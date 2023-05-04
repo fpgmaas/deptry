d1233358d250b2fb99fbc366f239421d99aa09c5
Mathieu Kniewallner <mathieu.kniewallner@gmail.com>
2023-05-04 23:24:24 +0200

    chore: run `pre-commit run -a`

--- deptry/compat.py
+++ deptry/compat.py
@@ -1,12 +1,6 @@
 from __future__ import annotations
 
-import sys
-
-if sys.version_info >= (3, 8):
-    import importlib.metadata as metadata
-    from importlib.metadata import PackageNotFoundError
-else:
-    import importlib_metadata as metadata
-    from importlib_metadata import PackageNotFoundError
+import importlib.metadata as metadata
+from importlib.metadata import PackageNotFoundError
 
 __all__ = ("metadata", "PackageNotFoundError")
