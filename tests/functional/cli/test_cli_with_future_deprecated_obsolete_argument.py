7e02fc0e63f10ebe52d9198ac9cbcd71029f3fa9
Florian Maas <fpgmaas@gmail.com>
2023-05-09 08:21:58 +0200

    Added assertion for warning log when obsolete argument is used

--- tests/functional/cli/test_cli_with_future_deprecated_obsolete_argument.py
+++ tests/functional/cli/test_cli_with_future_deprecated_obsolete_argument.py
@@ -1,11 +1,10 @@
 from __future__ import annotations
 
+import shlex
+import subprocess
 from pathlib import Path
 from typing import TYPE_CHECKING
 
-from click.testing import CliRunner
-
-from deptry.cli import deptry
 from tests.utils import get_issues_report, run_within_dir
 
 if TYPE_CHECKING:
@@ -13,12 +12,20 @@ if TYPE_CHECKING:
 
 
 def test_cli_uses_both_obsolete_and_unused_flag_from_pyproject_toml(
-    poetry_project_builder: ToolSpecificProjectBuilder,
+    project_builder: ToolSpecificProjectBuilder,
 ) -> None:
-    with run_within_dir(poetry_project_builder("project_with_future_deprecated_obsolete_argument")):
-        result = CliRunner().invoke(deptry, ". -o report.json")
+    with run_within_dir(
+        project_builder("project_with_future_deprecated_obsolete_argument", "poetry install --no-interaction --no-root")
+    ):
+        result = subprocess.run(shlex.split("poetry run deptry . -o report.json"), capture_output=True, text=True)
 
-        assert result.exit_code == 1
+        assert (
+            "Warning: In an upcoming release, support for the `--ignore-obsolete` and `-io` command-line options and"
+            " the `ignore_obsolete` configuration parameter will be discontinued. Instead, use the `--ignore-unused`,"
+            " `-iu` options or the `ignore_unused` configuration parameter to achieve the desired behavior."
+            in result.stderr
+        )
+        assert result.returncode == 1
         assert get_issues_report() == [
             {
                 "error": {
@@ -59,11 +66,27 @@ def test_cli_uses_both_obsolete_and_unused_flag_from_pyproject_toml(
         ]
 
 
-def test_cli_skip_obsolete_argument_still_works(poetry_project_builder: ToolSpecificProjectBuilder) -> None:
-    with run_within_dir(poetry_project_builder("project_with_future_deprecated_obsolete_argument")):
-        result = CliRunner().invoke(deptry, ". --skip-obsolete -o report.json")
+def test_cli_skip_obsolete_argument_still_works(project_builder: ToolSpecificProjectBuilder) -> None:
+    with run_within_dir(
+        project_builder("project_with_future_deprecated_obsolete_argument", "poetry install --no-interaction --no-root")
+    ):
+        result = subprocess.run(
+            shlex.split("poetry run deptry . --skip-obsolete -o report.json"), capture_output=True, text=True
+        )
 
-        assert result.exit_code == 1
+        assert (
+            "Warning: In an upcoming release, support for the `--ignore-obsolete` and `-io` command-line options and"
+            " the `ignore_obsolete` configuration parameter will be discontinued. Instead, use the `--ignore-unused`,"
+            " `-iu` options or the `ignore_unused` configuration parameter to achieve the desired behavior."
+            in result.stderr
+        )
+        assert (
+            "Warning: In an upcoming release, support for the `--skip-obsolete` command-line option and the"
+            " `skip_obsolete` configuration parameter will be discontinued. Instead, use the `--skip-unused` option or"
+            " the `skip_unused` configuration parameter to achieve the desired behavior."
+            in result.stderr
+        )
+        assert result.returncode == 1
         assert get_issues_report() == [
             {
                 "error": {
