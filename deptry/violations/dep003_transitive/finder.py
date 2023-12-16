61ddbaf92cf77a853443dc7beb3b0d0f15a55519
Mathieu Kniewallner <mathieu.kniewallner@gmail.com>
2023-12-16 20:29:44 +0100

    chore: run `pre-commit run ruff-format -a`

--- deptry/violations/dep003_transitive/finder.py
+++ deptry/violations/dep003_transitive/finder.py
@@ -42,14 +42,12 @@ class DEP003TransitiveDependenciesFinder(ViolationsFinder):
         return transitive_dependencies
 
     def _is_transitive(self, module: Module) -> bool:
-        if any(
-            [
-                module.package is None,
-                module.is_provided_by_dependency,
-                module.is_provided_by_dev_dependency,
-                module.local_module,
-            ]
-        ):
+        if any([
+            module.package is None,
+            module.is_provided_by_dependency,
+            module.is_provided_by_dev_dependency,
+            module.local_module,
+        ]):
             return False
 
         if module.name in self.ignored_modules:
