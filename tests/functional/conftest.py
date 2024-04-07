1b352e320163943945c31165f593b6c465897cc3
Mathieu Kniewallner <mathieu.kniewallner@gmail.com>
2024-04-07 11:51:23 +0200

    test(conftest): ensure that `pytest_sessionstart` hook runs once

--- tests/functional/conftest.py
+++ tests/functional/conftest.py
@@ -6,12 +6,20 @@ import sys
 from pathlib import Path
 
 import pytest
+import xdist
 
 from tests.functional.utils import DEPTRY_WHEEL_DIRECTORY
 from tests.utils import PDMVenvFactory, PipVenvFactory, PoetryVenvFactory
 
 
-def pytest_sessionstart() -> None:
+def pytest_sessionstart(session: pytest.Session) -> None:
+    # When running the tests on multiple workers with pytest-xdist, the hook will be run several times:
+    # - once from "master" node, when test suite starts
+    # - X times (where X is the number of workers), when tests start in each worker
+    # We only want to run the hook once, so we explicitly skip the hook if running on a pytest-xdist worker.
+    if xdist.is_xdist_worker(session):
+        return None
+
     deptry_wheel_path = Path(DEPTRY_WHEEL_DIRECTORY)
 
     print(f"Building `deptry` wheel in {deptry_wheel_path} to use it on functional tests...")  # noqa: T201
