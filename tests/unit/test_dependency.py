981bd1807a914c946a69b5081d89595b320b86d0
Mathieu Kniewallner <mathieu.kniewallner@gmail.com>
2024-09-14 13:15:59 +0200

    refactor(dependency): use `get_packages_from_distribution`

--- tests/unit/test_dependency.py
+++ tests/unit/test_dependency.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
-from importlib.metadata import PackageNotFoundError
 from pathlib import Path
 from unittest.mock import patch
 
@@ -21,20 +20,12 @@ def test_create_default_top_level_if_metadata_not_found() -> None:
     assert dependency.top_levels == {"foo_bar"}
 
 
-def test_read_top_level_from_top_level_txt() -> None:
+def test_get_top_levels_from_distribution() -> None:
     """
-    Read the top-levels.txt file
+    Get the packages from distribution.
     """
 
-    class MockDistribution:
-        def __init__(self) -> None:
-            pass
-
-        def read_text(self, file_name: str) -> str:
-            return "foo\nbar"
-
-    with patch("deptry.dependency.metadata.distribution") as mock:
-        mock.return_value = MockDistribution()
+    with patch("deptry.dependency.get_packages_from_distribution", return_value={"foo", "bar"}):
         dependency = Dependency("Foo-bar", Path("pyproject.toml"))
 
     assert dependency.name == "Foo-bar"
@@ -42,59 +33,25 @@ def test_read_top_level_from_top_level_txt() -> None:
     assert dependency.top_levels == {"foo", "bar"}
 
 
-def test_read_top_level_from_record() -> None:
+def test_get_top_levels_from_predefined() -> None:
     """
-    Verify that if top-level.txt not found, an attempt is made to extract top-level module names from
-    the metadata RECORD file.
+    Verify that if there are predefined top-level module names it take precedence over other lookup methods.
     """
-
-    class MockDistribution:
-        def __init__(self) -> None:
-            pass
-
-        def read_text(self, file_name: str) -> str | None:
-            if file_name == "RECORD":
-                return """\
-../../../bin/black,sha256=<HASH>,247
-__pycache__/_black_version.cpython-311.pyc,,
-_black_version.py,sha256=<HASH>,19
-black/trans.cpython-39-darwin.so,sha256=<HASH>
-black/trans.py,sha256=<HASH>
-blackd/__init__.py,sha256=<HASH>
-blackd/__main__.py,sha256=<HASH>
-                """
-            return None
-
-    with patch("deptry.dependency.metadata.distribution") as mock:
-        mock.return_value = MockDistribution()
-        dependency = Dependency("Foo-bar", Path("pyproject.toml"))
-
-    assert dependency.name == "Foo-bar"
-    assert dependency.definition_file == Path("pyproject.toml")
-    assert dependency.top_levels == {"_black_version", "black", "blackd"}
-
-
-def test_read_top_level_from_predefined() -> None:
-    """
-    Verify that if there are predefined top-level module names it takes
-    precedence over other lookup methods.
-    """
-    with patch("deptry.dependency.metadata.distribution") as mock:
+    with patch("deptry.dependency.get_packages_from_distribution") as mock:
         dependency = Dependency("Foo-bar", Path("pyproject.toml"), module_names=["foo"])
 
     assert dependency.name == "Foo-bar"
     assert dependency.definition_file == Path("pyproject.toml")
     assert dependency.top_levels == {"foo"}
-    mock.return_value.read_text.assert_not_called()
+    mock.assert_not_called()
 
 
-def test_not_predefined_and_not_installed() -> None:
+def test_get_top_levels_fallback() -> None:
     """
     Use the fallback option of translating the package name.
     """
 
-    with patch("deptry.dependency.metadata.distribution") as mock:
-        mock.side_effect = PackageNotFoundError
+    with patch("deptry.dependency.get_packages_from_distribution", return_value=None):
         dependency = Dependency("Foo-bar", Path("pyproject.toml"))
 
     assert dependency.name == "Foo-bar"
