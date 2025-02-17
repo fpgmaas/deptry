a16f9782b07c434a7f0c5a01aa440cda1e001879
Oren Me <oren.melamed@kaltura.com>
2025-02-17 18:06:12 +0200

    Update cli.py
    Co-authored-by: Florian Maas <fpgmaas@gmail.com>

--- python/deptry/cli.py
+++ python/deptry/cli.py
@@ -221,7 +221,7 @@ def display_deptry_version(ctx: click.Context, _param: click.Parameter, value: b
     "--github-output",
     "-go",
     is_flag=True,
-    help="""If specified, dependency issues found will be written in the format of github annotation. e.g. `deptry . -o deptry.json`""",
+    help="""If specified, dependency issues found will be written in the format of github annotation.""",
 )
 @click.option(
     "--github-warning-errors",
