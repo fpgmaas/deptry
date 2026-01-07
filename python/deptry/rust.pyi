8a731a3e685a16faa6ddb0c7d6fe86b790dd16ac
Tyler Riccio <tylerriccio8@gmail.com>
2026-01-06 21:17:19 -0500

    update python api

--- python/deptry/rust.pyi
+++ python/deptry/rust.pyi
@@ -5,7 +5,7 @@ from .rust import Location as RustLocation
 def get_imports_from_py_files(file_paths: list[str]) -> dict[str, list[RustLocation]]: ...
 def get_imports_from_ipynb_files(file_paths: list[str]) -> dict[str, list[RustLocation]]: ...
 def find_python_files(
-    directories: tuple[Path, ...],
+    paths: tuple[Path, ...],
     exclude: tuple[str, ...],
     extend_exclude: tuple[str, ...],
     using_default_exclude: bool,
